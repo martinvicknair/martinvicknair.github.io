c9070c214ee4b7f355d2a5133332b91fd5f06ab4
Martin Vicknair <martinvicknair@gmail.com>
2019-04-07 11:14:59 -0600

    simple html

--- backup/assets/js/main.js
+++
@@ -1,9 +0,0 @@
-jQuery(document).ready(function($) {
-
-    /* Github Calendar - https://github.com/IonicaBizau/github-calendar */
-    GitHubCalendar("#github-graph", "martinvicknair");
-
-    /* Github Activity Feed - https://github.com/caseyscarborough/github-activity */
-    GitHubActivity.feed({ username: "martinvicknair", selector: "#ghfeed" });
-
-});
